de2b3ae3ba4dcdb0d51b4231f65a7931ff60ab3d
Daniel <dan.costaa.12@gmail.com>
2024-04-30 14:31:00 +0100

    module1

--- UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -11,8 +11,8 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
 /** HelloWorld
   * An application with two maps side-by-side zoomed in on different locations.
   * Author: UC San Diego Coursera Intermediate Programming team
-  * @author Your name here
-  * Date: July 17, 2015
+  * @author Daniel Costa
+  * Date: April 24, 2024
   * */
 public class HelloWorld extends PApplet
 {
@@ -27,7 +27,7 @@ public class HelloWorld extends PApplet
 	public static String mbTilesString = "blankLight-1-3.mbtiles";
 	
 	// IF YOU ARE WORKING OFFLINE: Change the value of this variable to true
-	private static final boolean offline = false;
+	private static final boolean offline = true;
 	
 	/** The map we use to display our home town: La Jolla, CA */
 	UnfoldingMap map1;
@@ -65,14 +65,17 @@ public class HelloWorld extends PApplet
 		// The 6th argument specifies the map provider.  
 		// There are several providers built-in.
 		// Note if you are working offline you must use the MBTilesMapProvider
-		map1 = new UnfoldingMap(this, 50, 50, 350, 500, provider);
+		//map1 = new UnfoldingMap(this, 50, 50, 350, 500, provider);
+		map2 = new UnfoldingMap(this,50,50,350,500,provider);
 
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
-	    map1.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
+		//map1.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
+		map2.zoomAndPanTo(zoomLevel, new Location(38.676172800873, -9.164910015743638));
 		
 		// This line makes the map interactive
-		MapUtils.createDefaultEventDispatcher(this, map1);
+		//MapUtils.createDefaultEventDispatcher(this, map1);
+		MapUtils.createDefaultEventDispatcher(this, map2);
 		
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
@@ -83,7 +86,8 @@ public class HelloWorld extends PApplet
 	public void draw() {
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
-		map1.draw();
+		//map1.draw();
+		map2.draw();
 	}
 
 	
